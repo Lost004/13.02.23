9f10ec55b17014a4565c97517b09c98dd341593f
Lost004 <nazimri@code.edu.com>
2023-02-13 11:07:05 -0800

    13.02.23

---
+++ 13,02,23/Icourse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _13_02_23
+{
+	internal class Icourse
+	{
+		Group[] AddGroup(Group group);
+		Group GetGroupByNo(string no);
+		Group[] GetGroupsByPointRange(int minPoint, int maxPoint);
+		Group[] Search(string str);
+	}
+}
